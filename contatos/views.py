a111a471fd14082f984ffe9fad64eed042ce9055
root <root@DESKTOP-8C65REV.localdomain>
2022-05-10 16:52:33 -0400

    admin_upd13

--- contatos/views.py
+++ contatos/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Contato
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    contatos = Contato.objects.all()
+    contatos = Contato.objects.order_by()
+    paginator = Paginator(contatos, 20)
+
+    page = request.GET.get('p')
+    contatos = paginator.get_page(page)
+
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
@@ -12,6 +18,10 @@ def index(request):
 def ver_contato(request, contato_id):
     # contato = Contato.objects.get(id=contato_id)
     contato = get_object_or_404(Contato, id=contato_id)
+
+    if not contato.mostrar:
+        raise Http404()
+
     return render(request, "contatos/ver_contato.html", {
         'contato': contato
     })
