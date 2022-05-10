443c2fa2b21c15b32efa049a42b21638cfe94a28
root <root@DESKTOP-8C65REV.localdomain>
2022-05-10 14:38:33 -0400

    admin_upd4

--- contatos/admin.py
+++ contatos/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import Categoria, Contato
 
 # Register your models here.
+
+class ContatoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'sobrenome')
+
 admin.site.register(Categoria)
+admin.site.register(Contato, ContatoAdmin)
--- contatos/models.py
+++ contatos/models.py
@@ -17,3 +17,6 @@ class Contato(models.Model):
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.nome
