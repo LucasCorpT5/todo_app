e7de2719553ff1e0f7d63893ae70aa501e3882bf
root <root@DESKTOP-8C65REV.localdomain>
2022-05-10 14:05:31 -0400

    urls_pattern_upd5

--- contatos/models.py
+++ contatos/models.py
@@ -3,9 +3,13 @@ from django.utils import timezone
 
 # Create your models here.
 class Contato(models.Model):
+    # blank=True significa que o campo não é obrigatorio
     nome = models.CharField(max_length=255)
-    sobrenome = models.CharField(max_length=255, blank=True)
+    sobrenome = models.CharField(max_length=255, blank=True) 
     telefone = models.CharField(max_length=255)
     email = models.CharField(max_length=255, blank=True)
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=255)
