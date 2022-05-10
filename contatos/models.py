895e69a4a0121ee4729696c7a4215ec91a83517a
root <root@DESKTOP-8C65REV.localdomain>
2022-05-10 14:02:29 -0400

    urls_pattern_upd3

--- contatos/models.py
+++ contatos/models.py
@@ -5,4 +5,4 @@ class Contato(models.Model):
     nome = models.CharField(max_length=255)
     sobrenome = models.CharField(max_length=255, blank=True)
     telefone = models.CharField(max_length=255)
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, blank=True)
