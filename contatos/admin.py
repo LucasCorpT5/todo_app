41f128b3560a4e663be0d3eda8d2408de44bd6bb
root <root@DESKTOP-8C65REV.localdomain>
2022-05-10 14:48:18 -0400

    admin_upd6

--- contatos/admin.py
+++ contatos/admin.py
@@ -5,6 +5,9 @@ from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'data_criacao', 'categoria')
+    list_display_links = ('id', 'nome', 'sobrenome')
+    # list_filter = ('nome', 'sobrenome')
+    list_per_page = 10
 
 admin.site.register(Categoria)
 admin.site.register(Contato, ContatoAdmin)
