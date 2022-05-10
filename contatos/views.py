9fcd07248461cdf24760cf95ea167c303ec3322b
root <root@DESKTOP-8C65REV.localdomain>
2022-05-10 15:40:06 -0400

    admin_upd7

--- contatos/views.py
+++ contatos/views.py
@@ -7,3 +7,9 @@ def index(request):
     return render(request, 'contatos/index.html', {
         'contatos': contatos
     })
+
+def ver_contato(request, contato_id):
+    contato = Contato.objects.get(id=contato_id)
+    return render(request, "contatos/ver_contato.html", {
+        'contato': contato
+    })
