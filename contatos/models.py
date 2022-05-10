6be3dc4064df14b25aa05ea6944b48f28d26ebf6
root <root@DESKTOP-8C65REV.localdomain>
2022-05-10 14:06:08 -0400

    modelcreate_upd6

--- contatos/models.py
+++ contatos/models.py
@@ -10,6 +10,7 @@ class Contato(models.Model):
     email = models.CharField(max_length=255, blank=True)
     data_criacao = models.DateTimeField(default=timezone.now)
     descricao = models.TextField(blank=True)
+    categoria = models.ForeignKey()
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=255)
